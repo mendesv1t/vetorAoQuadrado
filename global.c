cd07009954900d0dc5ff13df70e83334d9b8a6ef
Vitória Chaves <vitoriagrier@gmail.com>
2024-03-24 11:32:21 -0300

    altera quantidade de inteiros e threads

--- global.c
+++ global.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 
 // variáveis globais, que serão compartilhadas com os fluxos de execução criados:
-const int M = 2; // número de threads
-const int N = 10; // número de elementos do vetor
+const int M = 4; // número de threads
+const int N = 100; // número de elementos do vetor
 int * vetor = NULL; // declarando vetor de inteiros.
 int * vetorAux = NULL; // declarando vetor auxiliar de inteiros.
